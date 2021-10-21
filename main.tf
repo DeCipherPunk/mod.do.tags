da571b4063ef592f10a7f5ab933e46d5b1ea5eba
DeCipherPunk <decipherpunk@protonmail.com>
2021-10-21 19:05:44 +0530

    fix vars & add provider

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+    required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+ provider "digitalocean" {
+    token = "${var.do_token}"
+ }
 resource "digitalocean_tag" "tag_name" {
   name = "${var.tag_name}"
 }
